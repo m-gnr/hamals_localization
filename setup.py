89dd8bc7521c8912c0785438b3d82523309c3e7d
m_gnr <m_gnr@icloud.com>
2026-02-09 01:40:43 +0300

    robot ekf files added

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'hamals_localization'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+
         ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py')),
+
+        (os.path.join('share', package_name, 'config'),
+            glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='m_gnr',
     maintainer_email='m_gnr@icloud.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-        ],
-    },
+    description='Localization EKF package (robot_localization)',
+    license='MIT',
 )
